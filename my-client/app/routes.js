d20c984ceae2180b62dc71079030a653e82557b3
mokhif <dunosixen@gmail.com>
2026-08-13 15:00:45 +0100

    removed the loader

--- my-client/app/routes.js
+++ my-client/app/routes.js
@@ -1,26 +1,7 @@
-import { route } from "@react-router/dev/routes";
-import { json, redirect } from "react-router";
-import jwt from "jsonwebtoken";
-
-const requireAuth = async ({ request }) => {
-  const cookieHeader = request.headers.get("cookie") || "";
-  const token = cookieHeader
-    .split("; ")
-    .find((c) => c.startsWith("token="))
-    ?.split("=")[1];
-
-  if (!token) return redirect("/register");
-
-  try {
-    jwt.verify(token, process.env.JWT_SECRET);
-    return json({ authenticated: true });
-  } catch {
-    return redirect("/register");
-  }
-};
+import { index, route } from "@react-router/dev/routes";
 
 export default [
-  { index: true, loader: requireAuth, Component: "./pages/home.jsx" },
+  index("./pages/home.jsx"),
   route("login", "./pages/login.jsx"),
   route("register", "./pages/register.jsx"),
 ];
