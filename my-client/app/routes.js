55a88df9cd02a488bb8e6b5b7cc2a9b845ad7430
mokhif <dunosixen@gmail.com>
2026-08-13 14:43:37 +0100

    fixed some route issues

--- my-client/app/routes.js
+++ my-client/app/routes.js
@@ -1,28 +1,26 @@
-// app/routes.js
-import { index, route } from "@react-router/dev/routes";
+import { route } from "@react-router/dev/routes";
 import { json, redirect } from "react-router";
-import Home from "./pages/home.jsx";
-
 import jwt from "jsonwebtoken";
 
-const = async ({ request }) => {
-  const cookieHeader = request.header.get("cookie") || "";
+const requireAuth = async ({ request }) => {
+  const cookieHeader = request.headers.get("cookie") || "";
   const token = cookieHeader
     .split("; ")
     .find((c) => c.startsWith("token="))
     ?.split("=")[1];
-  if (!token) return json({ authenticated: false });
+
+  if (!token) return redirect("/register");
+
   try {
     jwt.verify(token, process.env.JWT_SECRET);
-    return redirect("/home");
+    return json({ authenticated: true });
   } catch {
-    return json({ authenticated: false });
+    return redirect("/register");
   }
 };
 
 export default [
-  {index:true,loader:requireAuth,Component:()=> null},
+  { index: true, loader: requireAuth, Component: "./pages/home.jsx" },
   route("login", "./pages/login.jsx"),
   route("register", "./pages/register.jsx"),
-  route("/", "./pages/home.jsx"),
 ];
